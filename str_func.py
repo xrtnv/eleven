f0f9b03ac340eb87c63b45d704a25eb755869f6d
xrtnv <xrtnv@xrtnv.com>
2023-12-10 20:55:50 +0300

    DOCSTRINGS and new func

--- str_func.py
+++ str_func.py
@@ -1,2 +1,9 @@
 def toUpper(a):
+    '''Returnes uppercased word.'''
     return a.upper()
+
+
+def secondToUpper(a):
+    '''Returnes capitalized words.'''
+    a = ' '.join(word.capitalize() for word in a.split())
+    print(a)
